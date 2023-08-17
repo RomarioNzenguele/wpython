ba60f18a18a52682b919745a34c5fb5b4e6a6a96
“Romário <nz.romariodasilva@gmail.com>
2023-08-17 14:18:44 +0100

    Add Strings and Numeros class

---
+++ logica/Numeros.py
@@ -0,0 +1,21 @@
+# Operações Aritméticas
+
+print(4 + 4)  # Soma
+print(3 - 4)  # Subtração
+print(4 * 2)  # Multiplicação
+print(4 / 3)  # Divisão
+print(4 ** 2)  # Potência
+print(10 % 3)  # Módulo
+
+
+# Operações com Números Float
+
+print(10/4)  # Resultado é um número float
+print(10//4)  # Resultado apenas a parte inteira
+
+
+# Casting
+x = float(9)  # 9.0
+x = int(6.5)  # 6
+x = int(float('2.5'))  # 2
+print(x)
---
+++ logica/Strings.py
@@ -0,0 +1,34 @@
+# Declaracao
+a = """Python is good language.
+For many professionals, is the best language to start."""
+print(a)
+
+# Indexacao [Slicing]
+a = "Romario Nzenguele!"
+
+print(a[1])  # imprimindo o segundo caracter da string -> o
+print(a[7])  # imprime o oitavo caracter -> espaco
+print(a[2:5])  # imprime do terceiro ate ao quinto caracter -> mar
+print(a[:5])  # imprime do primeiro ate ao quinto caracter -> Romar
+print(a[2:])  # imprime do terceiro caracter ate ao ultimo -> mario Nzenguele!
+print(a[:-3])  # imprime sem os tres ultimos caracteres -> Romario Nzengue
+print(a[-4:])  # imprime sem os ultimos quatro caracteres  -> ele!
+print(a[-4:-3])  # dos quatro ultimos caracteres, elimina os tres ultimos -> e
+print(a[::-1])  # imprimindo de forma reversa
+
+# Modificando strings
+a = " Da Silva "
+print(a.upper())
+print(a.lower())
+print(a.strip())  # REMOVER espacos em brancos, semelhante ao trim() em java
+print(a.replace("a", "O"))  # SUBSTITUIR 'a' por 'O'
+
+a = "Pyton,Java,C#,JavaScript"
+print(a.split(","))  # RETORNA uma lista de acordo com o caracter especificado
+
+
+# Formatacao
+qtd = 3
+pu = 300
+msg = 'Kit = quantidade x pu = {} x {} = {} AKz'.format(qtd, pu, pu*qtd)
+print(msg)
