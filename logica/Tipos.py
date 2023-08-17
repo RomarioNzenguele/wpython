2c70c607a466b2ab84aec286093de7f5bbc88a12
“Romário <nz.romariodasilva@gmail.com>
2023-08-17 12:57:21 +0100

    Add. Tipos

---
+++ logica/Tipos.py
@@ -0,0 +1,49 @@
+"""
+TIPOS DE DADOS EM PYTHON
+
+Texto:	str
+numeros:	int, float, complex
+Sequencias:	list, tuple, range
+Mapas:	dict
+Tipo Set:	set, frozenset
+Boleanos:	bool
+Binarios:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+
+# aplicando todos os tipos
+
+x = "Hello World"  # str
+x = 20  # int
+x = 20.5  # float
+x = 1j  # complex
+x = ["apple", "banana", "cherry"]  # list
+x = ("apple", "banana", "cherry")  # tuple
+x = range(6)  # range
+x = {"name": "John", "age": 36}  # dict
+x = {"apple", "banana", "cherry"}  # set
+x = frozenset({"apple", "banana", "cherry"})  # frozenset
+x = True  # bool
+x = b"Hello"  # bytes
+x = bytearray(5)  # bytearray
+x = memoryview(bytes(5))  # memoryview
+x = None  # NoneType
+
+# verificacao de tipo
+print(type(x))  # retornara o ultimo tipo aplicado
+
+# Setando valores
+x = str("Hello World")  # str
+x = int(20)  # int
+x = float(20.5)  # float
+x = complex(1j)  # complex
+x = list(("apple", "banana", "cherry"))  # list
+x = tuple(("apple", "banana", "cherry"))  # tuple
+x = range(6)  # range
+x = dict(name="John", age=36)  # dict
+x = set(("apple", "banana", "cherry"))  # set
+x = frozenset(("apple", "banana", "cherry"))  # frozenset
+x = bool(5)  # bool
+x = bytes(5)  # bytes
+x = bytearray(5)  # bytearray
+x = memoryview(bytes(5))  # memoryview
