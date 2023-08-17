564ce75b11469458dbd9f4c347e5279d80eabbe1
“Romário <nz.romariodasilva@gmail.com>
2023-08-17 12:48:22 +0100

    Add. Variaveis

---
+++ logica/Variaveis.py
@@ -0,0 +1,30 @@
+# Definindo variaveis
+idade = 31
+preco = 12.45
+sexo = 'M'
+inteiro = 2_000_000_000  # 2 bilhoes
+longo = 9_000_000_000_000_000_000  # 9 quintilhoes
+
+print("Idade: ", idade)
+print(longo)
+
+# Declaração Múltipla
+x, y, z = "Azul", "Laranja", "Verde"
+print(y)  # imprimindo a cor laranja
+
+# Casting
+x = str(3)    # convertendo em string  => '3'
+y = int(3)    # convertendo em inteiro =>   3
+z = float(3)  # convertendo em float => 3.0
+
+# Concatenando com o print()
+print("Romario", "Nzenguele", "Da Silva")
+print("Romario " + "Nzenguele " + "Da Silva")
+
+# Operações com Variáveis
+numero1, numero2 = 5, 15
+print(str(numero1 + numero2))
+print(str(numero1 - numero2))
+print(str(numero1 * numero2))
+print(str(numero1 / numero2))
+print(str(numero1 % numero2))
